e79a3c39e54fadbda8bdcbcc7cd2daead4e4c5e5
Yasmin Underdown <yunderdo@ford.com>
2021-10-29 11:04:45 +0100

    Refactoring to use Paramaterized Tests

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -1,35 +1,18 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MarsRoverTest {
 
-    @Test
-    public void moveForwardNorthBy1() {
-        MarsRover marsRover = new MarsRover(0, 0, "N");
+    @ParameterizedTest
+    @CsvSource({"0,0,N,0,1", "0,0,E,1,0", "1,1,S,1,0", "1,1,W,0,1"})
+    public void movingForward(int x, int y, String direction, int expectedX, int expectedY) {
+        MarsRover marsRover = new MarsRover(x, y, direction);
         marsRover.execute("F");
-        assertEquals(1, marsRover.getY());
-    }
-
-    @Test
-    public void moveForwardEastBy1() {
-        MarsRover marsRover = new MarsRover(0, 0, "E");
-        marsRover.execute("F");
-        assertEquals(1, marsRover.getX());
-    }
-
-    @Test
-    public void moveForwardSouthBy1() {
-        MarsRover marsRover = new MarsRover(1,1, "S");
-        marsRover.execute("F");
-        assertEquals(0, marsRover.getY());
-    }
-
-    @Test
-    public void moveForwardWestBy1() {
-        MarsRover marsRover = new MarsRover(1, 1, "W");
-        marsRover.execute("F");
-        assertEquals(0, marsRover.getX());
+        assertEquals(expectedX, marsRover.getX());
+        assertEquals(expectedY, marsRover.getY());
     }
 
     @Test
