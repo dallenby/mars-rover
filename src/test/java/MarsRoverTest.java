78a6bbf5cfe5c90079604b52c867834e66f1074c
Ralph Mallett <rmallet6@ford.com>
2021-10-29 11:28:04 +0100

    Add parameterized test for moving backwards

--- src/test/java/MarsRoverTest.java
+++ src/test/java/MarsRoverTest.java
@@ -7,7 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class MarsRoverTest {
 
     @ParameterizedTest
-    @CsvSource({"0,0,N,0,1", "0,0,E,1,0", "1,1,S,1,0", "1,1,W,0,1"})
+    @CsvSource({
+            "0,0,N,0,1",
+            "0,0,E,1,0",
+            "1,1,S,1,0",
+            "1,1,W,0,1"
+    })
     public void movingForward(int x, int y, String direction, int expectedX, int expectedY) {
         MarsRover marsRover = new MarsRover(x, y, direction);
         marsRover.execute("F");
@@ -15,31 +20,18 @@ public class MarsRoverTest {
         assertEquals(expectedY, marsRover.getY());
     }
 
-    @Test
-    public void moveBackwardsFacingNorthBy1() {
-        MarsRover marsRover = new MarsRover(1, 1, "N");
+    @ParameterizedTest
+    @CsvSource({
+            "1,1,N,1,0",
+            "0,0,S,0,1",
+            "1,1,E,0,1",
+            "0,0,W,1,0"
+    })
+    public void moveBackwards(int x, int y, String direction, int expectedX, int expectedY) {
+        MarsRover marsRover = new MarsRover(x, y, direction);
         marsRover.execute("B");
-        assertEquals(0, marsRover.getY());
+        assertEquals(expectedX, marsRover.getX());
+        assertEquals(expectedY, marsRover.getY());
     }
 
-    @Test
-    public void moveBackwardsFacingSouthBy1() {
-        MarsRover marsRover = new MarsRover(0, 0, "S");
-        marsRover.execute("B");
-        assertEquals(1, marsRover.getY());
-    }
-
-    @Test
-    public void moveBackwardsFacingEastBy1() {
-        MarsRover marsRover = new MarsRover(1,1, "E");
-        marsRover.execute("B");
-        assertEquals(0, marsRover.getX());
-    }
-
-    @Test
-    public void moveBackwardsFacingWestBy1() {
-        MarsRover marsRover = new MarsRover(0,0, "W");
-        marsRover.execute("B");
-        assertEquals(1, marsRover.getX());
-    }
 }
